d5c617065f5720fea225a054f8f0811b9fd6f723
Emm Kwami <mapps@Kwamis-MacBook.local>
2022-09-13 15:28:13 -0700

    new moralis keys added

--- src/index.js
+++ src/index.js
@@ -5,8 +5,8 @@ import App from './App';
 import { MoralisProvider } from 'react-moralis';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-const appId = "Zr85SXS6pJ5juuJoW2xd81h4zQohzmtIP8e9TIFP"
-const serverUrl="https://siljlrtx7x0a.usemoralis.com:2053/server"
+const appId = "Hc7duO9epvHaRxPKNjlafT6SRfMmAobBUhITYSWe"
+const serverUrl="https://treqgmp8uplh.usemoralis.com:2053/server"
 
 
 
